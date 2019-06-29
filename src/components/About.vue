a8d638113996f6cf623ff5ffd449fe19b7d012fc
Alex Nye <galexynye@gmail.com>
2019-06-29 15:02:47 +0200

    Change about to even more better

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,10 +5,11 @@
         </div>
         <div class="section">
             <div class="section-column story">
-                <p>Hi, my name is Alex Nye. I'm a composer for film / games and an independent producer / artist. Check out my <a style="color:green" href="http://www.grimmforestmusic.com">studio website here.</a>
+                <p>Hi, my name is Alex Nye. I'm a composer for film/games and an independent artist-producer.
                     <!-- sound engineer, music teacher and founder of Music Sequencing, a website/youtube channel/store for some of my music products. -->
-                </p>
-                <p>I teach music production over the internet on MusicSequencing.com and release original songs under the name Saywell. You can listen to some of them below.</p>
+                <!-- </p> -->
+                <!-- <p> -->
+                    I teach music production over the internet on MusicSequencing.com and release original songs under the name Saywell. You can listen to some of my songs below or check out my <a style="color:green" href="http://www.grimmforestmusic.com">studio website here</a> for more info about working together.</p>
                        <div>
             <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/814433031&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
         </div>
